27a177faabf64a5042bbef26048145711d7cf8a6
haveswing <mr.osbourne@gmail.com>
2016-11-16 23:16:47 +0100

    Add files via upload

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -9,7 +9,7 @@ def handle(self, msg):
     flavor = telepot.flavor(msg)
 
     summary = telepot.glance(msg, flavor=flavor)
-    print (flavor, summary)
+    print(flavor, summary)
     
     chat_id = msg['from']['id']
     user_name = "%s %s" % (msg['from']['first_name'], msg['from']['last_name'])
@@ -18,9 +18,9 @@ def handle(self, msg):
     if command == "/rank":
         rank()
     elif command == "/start":
-        self.sendMessage(chat_id, "Welcome to KGS Rank Bot!", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+        bot.sendMessage(chat_id, "Welcome to KGS Rank Bot!", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     elif command == "/info":
-        self.sendMessage(chat_id, "INFO_HERE", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+        bot.sendMessage(chat_id, "INFO_HERE", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     
 def rank(self, msg):
     chat_id = msg['from']['id']
@@ -31,14 +31,14 @@ def rank(self, msg):
     graphUrl = "https://www.gokgs.com/servlet/graph/" + kgsUser + "-en_US.png"
     print(graphUrl)
     
-    self.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+    bot.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     
     
 TOKEN = sys.argv[1]  # get token from command-line
 
 bot = telepot.Bot("269117423:AAH83p9Qhllcu9KbloxeUzglOfIWw-Orwvg")
 bot.message_loop(handle)
-print ("Listening ...")
+print("Listening ...")
 
 # Keep the program running.
 while 1:
