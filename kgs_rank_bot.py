4c48e9f42dee0f7e54ca09af90bf4131f064b806
haveswing <mr.osbourne@gmail.com>
2016-12-26 14:17:10 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -18,9 +18,9 @@ def handle(msg):
     if command.startswith('/rank '):
         rank(msg)
     elif command == '/start':
-        bot.sendMessage(chat_id, "Welcome to KGS Rank Bot!", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+        bot.sendMessage(chat_id, 'Welcome to KGS Rank Bot!', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     elif command == '/info':
-        bot.sendMessage(chat_id, "INFO\nHERE", parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+        bot.sendMessage(chat_id, 'INFO\nHERE', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     
 def rank(msg):
     chat_id = msg['chat']['id']
@@ -36,7 +36,7 @@ def rank(msg):
     
 TOKEN = sys.argv[0]  # get token from command-line
 
-bot = telepot.Bot("269117423:AAH83p9Qhllcu9KbloxeUzglOfIWw-Orwvg")
+bot = telepot.Bot('269117423:AAH83p9Qhllcu9KbloxeUzglOfIWw-Orwvg')
 bot.message_loop(handle)
 bot.setWebhook()  # unset webhook by supplying no parameter
 print 'Listening ...'
