f2e11725a63822a23d6dbb359ec37acb7e196fca
haveswing <mr.osbourne@gmail.com>
2016-12-27 13:40:43 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -1,7 +1,7 @@
 import sys
 import time
-import telepot
 import urllib
+import telepot
 
 def __init__(self):
     print 'KGS Rank Bot started.'
@@ -31,10 +31,9 @@ def rank(msg):
     kgsUser = msg['text'][6:]
     graphUrl = 'https://www.gokgs.com/servlet/graph/' + kgsUser + '-en_US.png'
     print graphUrl
-    yourGraph = graphUrl
     
     # bot.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
-    bot.sendPhoto(chat_id, yourGraph)
+    bot.sendPhoto(chat_id, graphUrl)
     
 TOKEN = sys.argv[0]  # get token from command-line
 
