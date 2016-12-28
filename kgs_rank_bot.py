b304520c803f5fa317ea6ddf8b4f0311568dc65f
haveswing <mr.osbourne@gmail.com>
2016-12-28 01:12:42 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -40,7 +40,7 @@ TOKEN = sys.argv[0]  # get token from command-line
 bot = telepot.Bot('269117423:AAH83p9Qhllcu9KbloxeUzglOfIWw-Orwvg')
 bot.message_loop(handle)
 bot.setWebhook()  # unset webhook by supplying no parameter
-print 'Listening ...'
+print 'KGS Rank Bot is listening...'
 
 # Keep the program running.
 while 1:
