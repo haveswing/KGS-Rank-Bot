acec6831d1324cddbc5ab5e2e3d6a11e79e31400
haveswing <mr.osbourne@gmail.com>
2016-12-29 21:40:45 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -36,6 +36,7 @@ def rank(msg):
     opener = urllib.FancyURLopener(proxies)
     theGraph = opener.open(graphUrl)
     theGraph.read()
+    print theGraph
     
     # bot.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     bot.sendPhoto(chat_id, theGraph, caption=('KGS rank graph for ' + kgsUser + '.'))
