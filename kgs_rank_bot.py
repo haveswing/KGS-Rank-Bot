3eb6167bf31b09e5b4771c507145074af231599b
haveswing <mr.osbourne@gmail.com>
2016-12-29 21:47:02 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -36,7 +36,7 @@ def rank(msg):
     opener = urllib.FancyURLopener(proxies)
     theGraph = opener.open(graphUrl)
     theGraph.read()
-    print 'ciao ' + theGraph + ' ciao'
+    print ('ciao ' , theGraph , ' ciao')
     
     # bot.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     bot.sendPhoto(chat_id, graphUrl, caption=('KGS rank graph for ' + kgsUser + '.'))
