197ad055665d9ac4020c3ec74613085ae969509d
haveswing <mr.osbourne@gmail.com>
2016-12-30 11:54:35 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -33,10 +33,8 @@ def rank(msg):
     graphUrl = 'https://www.gokgs.com/servlet/graph/' + kgsUser + '-en_US.png'
     print graphUrl
     
-    theGraph = urlopen(graphUrl)
-    
     # bot.sendPhoto(chat_id, graphUrl, caption=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
-    bot.sendPhoto(chat_id, theGraph, caption=('KGS rank graph for ' + kgsUser + '.'))
+    bot.sendPhoto(chat_id, graphUrl, caption=('KGS rank graph for ' + kgsUser + '.'))
     
 TOKEN = sys.argv[0]  # get token from command-line (was 1)
 
