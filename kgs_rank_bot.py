26abf4ae24c94bc31fa0871a38d56c9645fcfe1e
haveswing <mr.osbourne@gmail.com>
2017-01-09 14:24:43 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -41,7 +41,7 @@ def rank(msg):
     
     theGraph = urllib2.urlopen(graphUrl)
     
-    if msg['id'] == 220280982666:
+    if msg['id'] == '220280982666':
     
         bot.sendPhoto(chat_id, ('rankgraph.png', theGraph), caption=('KGS rank graph for ' + kgsUser + '.'))
     
