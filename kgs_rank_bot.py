53bb2367d5f8124a9a40f3436f9154d5cb41fe7c
haveswing <mr.osbourne@gmail.com>
2017-01-10 02:31:15 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -18,29 +18,37 @@ def handle(msg):
     
     if command.startswith('/rank '):
         rank(msg)
+    
     elif command == '/rank':
         howtorank(msg)
+    
     elif command == '/rank@kgsrankbot':
         howtorank(msg)
+    
     elif command == '/start':
         print chat_id, 'started KGS Rank Bot.'
         bot.sendMessage(chat_id, 'Hello! Here KGS Rank Bot, may the sente be with you.', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+    
     elif command == '/start@kgsrankbot':
         print chat_id, 'started KGS Rank Bot.'
         bot.sendMessage(chat_id, 'Hello! Here KGS Rank Bot, may the sente be with you.', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+    
     elif command == '/info':
         print chat_id, 'request info.'
         dev = urllib2.urlopen('https://www.gokgs.com/servlet/graph/haveswing-en_US.png')
         bot.sendMessage(chat_id, 'COMMANDS:\n/rank - Get the rank graph of a KGS player.\n/top100 - Top 100 KGS players.\n/info - About the bot...\n\nKGS RANK BOT ON GITHUB:\nhttps://github.com/haveswing/KGS-Rank-Bot\n\nKGS HOMEPAGE:\nhttps://www.gokgs.com/', parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
         bot.sendPhoto(chat_id, ('haveswing.png', dev), caption=('Developed by haveswing.\nantonio.romaggioli@gmail.com'))
+    
     elif command == '/info@kgsrankbot':
         print chat_id, 'request info.'
         dev = urllib2.urlopen('https://www.gokgs.com/servlet/graph/haveswing-en_US.png')
         bot.sendMessage(chat_id, 'COMMANDS:\n/rank - Get the rank graph of a KGS player.\n/top100 - Top 100 KGS players.\n/info - About the bot...\n\nKGS RANK BOT ON GITHUB:\nhttps://github.com/haveswing/KGS-Rank-Bot\n\nKGS HOMEPAGE:\nhttps://www.gokgs.com/', parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
         bot.sendPhoto(chat_id, ('haveswing.png', dev), caption=('Developed by haveswing.\nantonio.romaggioli@gmail.com'))
+    
     elif command == '/top100':
         print chat_id, 'request top100.'
         bot.sendMessage(chat_id, 'Top 100 KGS Players:\nhttps://www.gokgs.com/top100.jsp', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+    
     elif command == '/top100@kgsrankbot':
         print chat_id, 'request top100.'
         bot.sendMessage(chat_id, 'Top 100 KGS Players:\nhttps://www.gokgs.com/top100.jsp', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
