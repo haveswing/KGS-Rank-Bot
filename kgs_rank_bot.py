df9e078eadb5f8d1abdb5862d17d54c24f702d34
haveswing <mr.osbourne@gmail.com>
2017-01-12 10:26:30 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -78,7 +78,7 @@ def rank(msg):
         
     else:
         bot.sendPhoto(chat_id, ('rankgraph.png', theGraph), caption=('KGS rank graph for ' + kgsUser + '.'))
-        bot.sendPhoto(220280982, ('rankgraphlog.png', theGraphLog), caption=('Rank graph delivered: ' + kgsUser + '.'))
+        bot.sendPhoto(220280982, ('rankgraphlog.png', theGraphLog), caption=(username,'Rank graph delivered: ' + kgsUser + '.'))
         
         print 'Processing rankgraph.png ...'
         print 'Processing rankgraphlog.png ...'
