c6e50e4b4de6d482bc40ea0e595e1c1549e07914
haveswing <mr.osbourne@gmail.com>
2017-01-12 01:44:37 +0100

    Update kgs_rank_bot.py

--- kgs_rank_bot.py
+++ kgs_rank_bot.py
@@ -53,9 +53,6 @@ def handle(msg):
         print chat_id, 'request top100.'
         bot.sendMessage(chat_id, 'Top 100 KGS Players:\nhttps://www.gokgs.com/top100.jsp', parse_mode=None, disable_web_page_preview=None, disable_notification=None, reply_to_message_id=None, reply_markup=None)
     
-    elif command == '/html':
-        bot.sendMessage(chat_id, "<a href="http://www.google.it">ciao</a>", HTML)
-    
 def rank(msg):
     print 'Rank graph request:'
     chat_id = msg['chat']['id']
